dba4bd72b685d35962fdae94717cdc396ec002c7
Andrey <219523356+designedbyandrey@users.noreply.github.com>
2026-02-04 16:10:49 +0100

    Make the Rock Paper Scissors game functional with playGame and playRound functions

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-console.log("Hello World");
+
 
 function getComputerChoice() {
     let randomNumber= Math.random()
-    console.log (randomNumber);
+    // console.log (randomNumber);
     
     if (randomNumber > 0 && randomNumber <= 0.33) {
         return "Rock"
@@ -13,4 +13,55 @@ function getComputerChoice() {
     }
 
 }
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
+
+function getHumanChoice() {
+    return prompt("Your choice here")
+}
+
+//console.log(getHumanChoice())
+
+
+
+function playGame() {
+    let humanScore = 0
+    let computerScore = 0
+
+    for (let i = 0; i < 5 ; i++){
+    function playRound(humanChoice, computerChoice) {
+        let formattedHuman = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
+    
+        if (computerChoice === "Rock" && formattedHuman === "Paper" || computerChoice === "Paper" && formattedHuman === "Scissors" ||         
+        computerChoice === "Scissors" && formattedHuman === "Rock")  {
+        console.log(`You win ${formattedHuman} beats ${computerChoice}`);
+        return humanScore ++;
+        
+    
+    }   else if (computerChoice === "Rock" && formattedHuman === "Scissors" || computerChoice === "Paper" && formattedHuman === "Rock" || computerChoice === "Scissors" && formattedHuman === "Paper") { 
+        console.log(`You lose ${computerChoice} beats ${formattedHuman}`);
+        return computerScore ++;
+    }   else { 
+        console.log("Its a draw");
+        
+    }}
+
+
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+console.log(humanScore, computerScore);
+}
+if (humanScore > computerScore) {
+   console.log("You win the whole game") 
+} else if (humanScore < computerScore){
+    console.log('You lose the whole game')
+} else {
+    console.log ("The game is a draw")
+}
+}
+
+playGame()
