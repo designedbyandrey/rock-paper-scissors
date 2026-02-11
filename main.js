ededca519d3a4bc47339b93b08bd08c6503c2dfa
Andrey <219523356+designedbyandrey@users.noreply.github.com>
2026-02-11 14:52:34 +0100

    Add UI to the game and change js logic

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
+let humanScore = 0
+let computerScore = 0
+let rounds = 0
+
+
+
+let btnRock = document.querySelector("#rock")
+let btnPaper = document.querySelector('#paper')
+let btnSci = document.querySelector ('#sci')
+let resultField = document.querySelector("#results")
+let humanScoreOutput = document.querySelector("#human-score")
+let computerScoreOutput = document.querySelector("#computer-score")
+let endResult = document.querySelector("#end-result")
+
+
+
+function playGame() {
+  
+
+    btnRock.addEventListener('click', () => {
+    const computerSelection = getComputerChoice();
+    playRound("Rock", computerSelection);
+
+})
+
+btnPaper.addEventListener('click', () => {
+    const computerSelection = getComputerChoice();
+    playRound("Paper", computerSelection);
+
+})
+
+btnSci.addEventListener('click', () => {
+    const computerSelection = getComputerChoice();
+    playRound("Scissors", computerSelection);
+
+})
+
 
 
 function getComputerChoice() {
@@ -13,55 +50,62 @@ function getComputerChoice() {
     }
 
 }
-//console.log(getComputerChoice());
-
-function getHumanChoice() {
-    return prompt("Your choice here")
-}
-
-//console.log(getHumanChoice())
 
 
 
-function playGame() {
-    let humanScore = 0
-    let computerScore = 0
 
-    for (let i = 0; i < 5 ; i++){
-    function playRound(humanChoice, computerChoice) {
-        let formattedHuman = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
+function playRound(humanChoice, computerChoice) {
+    let formattedHuman = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
     
-        if (computerChoice === "Rock" && formattedHuman === "Paper" || computerChoice === "Paper" && formattedHuman === "Scissors" ||         
+    if (computerChoice === "Rock" && formattedHuman === "Paper" || computerChoice === "Paper" && formattedHuman === "Scissors" ||         
         computerChoice === "Scissors" && formattedHuman === "Rock")  {
-        console.log(`You win ${formattedHuman} beats ${computerChoice}`);
-        return humanScore ++;
-        
+        resultField.textContent = `You win ${formattedHuman} beats ${computerChoice}`;
+        humanScore += 1;
+        humanScoreOutput.textContent = humanScore.toString();
+        rounds +=1;
     
-    }   else if (computerChoice === "Rock" && formattedHuman === "Scissors" || computerChoice === "Paper" && formattedHuman === "Rock" || computerChoice === "Scissors" && formattedHuman === "Paper") { 
-        console.log(`You lose ${computerChoice} beats ${formattedHuman}`);
-        return computerScore ++;
-    }   else { 
-        console.log("Its a draw");
+    } else if (computerChoice === "Rock" && formattedHuman === "Scissors" || computerChoice === "Paper" && formattedHuman === "Rock" || computerChoice === "Scissors" && formattedHuman === "Paper") { 
+        resultField.textContent = `You lose ${computerChoice} beats ${formattedHuman}`;
+        computerScore +=1
+        computerScoreOutput.textContent = computerScore.toString();
+        rounds +=1;
+
+    } else { 
+        resultField.textContent = "Its a draw"
+        rounds +=1;
+    } 
+
+    if (rounds >= 5){
+         if (humanScore > computerScore) {
+        alert (`You win the whole game: Your score ${humanScore} computer score ${computerScore}`);
+        } else if (humanScore < computerScore) {
+            alert (`You loose the whole game: Your score ${humanScore} computer score ${computerScore}`);
+        } else {
+            alert (`The game is a draw: Your score ${humanScore} computer score ${computerScore}`);
+        }
+        resetGame()
         
-    }}
+
+    }
+
+}}
 
 
 
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
-
-console.log(humanScore, computerScore);
-}
-if (humanScore > computerScore) {
-   console.log("You win the whole game") 
-} else if (humanScore < computerScore){
-    console.log('You lose the whole game')
-} else {
-    console.log ("The game is a draw")
-}
-}
 
 playGame()
+
+
+function resetGame () {
+    humanScore = 0;
+    computerScore = 0;
+    rounds = 0;
+    humanScoreOutput.textContent = "0";
+    computerScoreOutput.textContent = "0";
+    resultField.textContent = "New Game! Make your move.";
+
+}
+
+
+
